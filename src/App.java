7146e049c54a5371164d01c060199d93a1bcb3f4
Prakash-Reddy08 <mjprakashreddy1@gmail.com>
2022-08-02 19:35:04 +0530

    complete mergesort

--- src/App.java
+++ src/App.java
@@ -16,10 +16,56 @@ public class App {
         printArray(numbers);
     }
 
-    public static void mergeSort(int[] arr) {
-        
+    public static void mergeSort(int[] inputArray) {
+        int inputLength = inputArray.length;
+        if (inputLength < 2) {
+            return;
+        }
+        int midIndex = inputLength / 2;
+        int[] leftHalf = new int[midIndex];
+        int[] rightHalf = new int[inputLength - midIndex];
+
+        for (int i = 0; i < midIndex; i++) {
+            leftHalf[i] = inputArray[i];
+        }
+        for (int i = midIndex; i < inputLength; i++) {
+            rightHalf[i - midIndex] = inputArray[i];
+        }
+        mergeSort(leftHalf);
+        mergeSort(rightHalf);
+        merge(inputArray, leftHalf, rightHalf);
     }
     
+    public static void merge(int[] inputArray, int[] leftHalf, int[] rightHalf) {
+        int leftSize = leftHalf.length;
+        int rightSize = rightHalf.length;
+
+        int i = 0; // ** is used to traverse the leftHalf array
+        int j = 0; // ** is used to traverse the rightHalf array
+        int k = 0; // ** is used to traverse the merged array
+
+        while (i < leftSize && j < rightSize) {
+            if (leftHalf[i] <= rightHalf[j]) {
+                inputArray[k] = leftHalf[i];
+                i++;
+            }else{
+                inputArray[k] = rightHalf[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<leftSize){
+            inputArray[k] = leftHalf[i];
+            i++;
+            k++;
+        }
+        while(j<rightSize){
+            inputArray[k] = rightHalf[j];
+            j++;
+            k++;
+        }
+    }
+
     public static void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + ", ");
